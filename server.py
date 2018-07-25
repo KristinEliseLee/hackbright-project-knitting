6304ef30b8e546aaae5414c9c5af141695cf4d83
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-24 17:34:01 -0700

    setting up file structure for project app

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import connect_to_db
@@ -6,3 +6,8 @@ from model import connect_to_db
 
 app = Flask(__name__)
 app.secret_key = 'ABCSECRETDEF'
+
+@app.route('/')
+def show_homepage():
+
+    return render_template('home.html')
