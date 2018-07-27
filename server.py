982c6dd4185d1f503949c453123d4233d3a3fc0f
KristinEliseLee <KristinEliseLee@gmail.com>
2018-07-27 00:55:05 +0000

    added registration form and functionality

--- server.py
+++ server.py
@@ -1,8 +1,29 @@
 from flask import Flask, render_template, flash, redirect, request, session
+
 from flask_debugtoolbar import DebugToolbarExtension
 import urllib
 
 from model import connect_to_db, User, Pattern, UserLikesPattern
+
+from wtforms import Form, BooleanField, StringField, PasswordField, validators, ValidationError
+
+class RegistrationForm(Form):
+
+    def validate_new_user(form, field):
+        if User.query.filter_by(email=field.data):
+            raise ValidationError('That email is taken')
+
+    email = StringField('Email Address', [
+        validators.Length(min=6, max=100),
+        validate_new_user])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match'),
+        validators.Length(min=6, max=50),
+    ])
+    confirm = PasswordField('Repeat Password')
+
+
 
 
 app = Flask(__name__)
@@ -13,6 +34,32 @@ connect_to_db(app, 'knitpreviewproject')
 def show_homepage():
 
     return render_template('home.html')
+
+
+@app.route('/register')
+def show_registration_form():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    """adds new user to the DB and logs them in"""
+    form = RegistrationForm(request.form)
+    if form.validate():
+        user = User(email=form.email.data, password=form.password.data)
+        db_session.add(user)
+
+        session['user_id'] = user.user_id
+        flash('Registered Successfully')
+        return redirect('/user')
+    return render_template('register.html', form=form)
 
 
 @app.route('/login')
